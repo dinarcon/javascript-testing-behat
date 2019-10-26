bd8751d9c63d5537805ab16b88fe00720d2ccef4
Mauricio Dinarte <drupal@dinarcon.com>
2019-10-25 19:30:59 -0600

    Add JavaScript test

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,22 +1,51 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Exception\ElementNotFoundException;
+use Behat\MinkExtension\Context\RawMinkContext;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+class FeatureContext extends RawMinkContext implements Context
 {
     /**
-     * Initializes context.
+     * @When I select the first autocomplete option for :prefix on the :field field
      *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * Based on code by Lyle Mantooth.
+     * @see https://gist.github.com/IslandUsurper/12723643dddc9315ff71
      */
-    public function __construct()
+    public function iSelectFirstAutocomplete($prefix, $field) {
+        $driver = $this->getSession()->getDriver();
+        $page = $this->getSession()->getPage();
+
+        $element = $page->findField($field);
+        if (!$element) {
+            throw new ElementNotFoundException($driver, NULL, 'named', $field);
+        }
+        $page->fillField($field, $prefix);
+
+        $this->getSession()->wait(1000, 'jQuery(".suggestions").is(":visible") === true');
+
+        $xpath = $element->getXpath();
+        // Down key.
+        $driver->keyDown($xpath, 40);
+        $driver->keyUp($xpath, 40);
+    }
+
+    /**
+     * @When I press the search button
+     */
+    public function iPressTheSearchButton()
     {
+        $this->getSession()->getPage()->pressButton('searchButton');
+    }
+
+    /**
+     * @Then first header of the page should be :text
+     */
+    public function firstHeaderOfThePageShouldBe($text)
+    {
+        $this->assertSession()->elementTextContains('css', 'h1', $text);
     }
 }
--- features/search.feature
+++ features/search.feature
@@ -9,3 +9,11 @@ Feature: Search
     And I press "searchButton"
     Then I should see "Pantheon (software), a web development platform"
     And I should see "Pantheon (desktop environment), a GTK+-based desktop environment"
+
+  @javascript
+  Scenario: Search for a term using autocomplete
+    Given I am on the homepage
+    When I select the first autocomplete option for "behat computer" on the "search" field
+    And I press the search button
+    Then first header of the page should be "Behat (computer science)"
+    And I should see "Behat is a test framework for behavior-driven development written in the PHP programming language."
